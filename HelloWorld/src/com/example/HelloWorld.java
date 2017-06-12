aba1b132419fdfa8cbf2f0240003224840d9d196
mjkhan <mjkhan21@gmail.com>
2017-06-12 23:11:05 +0900

    Delete all

--- HelloWorld/src/com/example/HelloWorld.java
+++
@@ -1,8 +0,0 @@
-package com.example;
-
-public class HelloWorld {
-	public static void main(String[] args) {
-		System.out.println("Hello, world!");
-		System.out.println("It's me!");
-	}
-}
--- HelloWorld2/src/com/example/HelloWorld2.java
+++
@@ -1,7 +0,0 @@
-package com.example;
-
-public class HelloWorld2 {
-	public static void main(String[] args) {
-		System.out.println("Hello, world, again!");
-	}
-}
